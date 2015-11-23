d14012dd189af38e3e485a7e07c07085d78e90a9
Robin Siep <RobinSiep1@gmail.com>
2015-11-23 14:29:57 +0100

    hardware info crawler done

--- pcbuilder_crawlers/spiders/hardware_info.py
+++ pcbuilder_crawlers/spiders/hardware_info.py
@@ -1,3 +1,5 @@
+import HTMLParser
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
@@ -15,6 +17,8 @@ allowed_urls = (
     "2/optischedrives"
 )
 
+html = HTMLParser.HTMLParser()
+
 
 class HardwareInfoSpider(CrawlSpider):
     name = "hardwareinfo"
@@ -25,6 +29,8 @@ class HardwareInfoSpider(CrawlSpider):
     # Extract product specification links, follow them and extract the result
     # to the parse method
     rules = (
+        Rule(LinkExtractor(restrict_xpaths="//ul[@class='pagination']"),
+             follow=True),
         Rule(LinkExtractor(restrict_xpaths=("//table[@id='productresulttable']"
                                             ),
                            allow=("specificaties",)),
@@ -34,18 +40,46 @@ class HardwareInfoSpider(CrawlSpider):
     def parse_item(self, response):
         # The key is always the value of the second td  in the specifications
         # table. The value belonging to the key is the third td.
-
         key_xpath = "td[position()=2]/text()"
         value_xpath = "td[position()=3]/text()"
+
+        # Append a before text() for values inside links
+        link_value_xpath = re.sub(r'(text())', r'a/\1', value_xpath)
+
+        # Hardware info uses an image with a tick that specifies if product has
+        # something. The alt specifies this in text in the form of "ja" or
+        # "nee"
+        tick_value_path = "{}/{}".format(value_xpath.split('/', 1)[0],
+                                         "img/@alt")
+
+        product = {}
+        product["category"] = html.unescape(''.join(response.xpath("//ul[@class='breadcrumb']\
+            /li[3]/a/text()").extract()))
 
         # Get the specifications div
         for sel in response.xpath(
                 "//*[@id='contentWithoutSidebar']/div/div[2]/div"):
-            product = {}
 
             # Select the tables and loop trough the rows
             for row in sel.xpath("table/tbody/tr"):
                 # Lookup the key and value and append them to the product
-                product[''.join(row.xpath(key_xpath).extract())] = \
-                    ''.join(row.xpath(value_xpath).extract())
-            yield product
+                key = html.unescape(''.join(row.xpath(key_xpath).extract()))
+                value = html.unescape(''.join(row.xpath(value_xpath)
+                                              .extract()).strip())
+
+                link_value = html.unescape(''.join(row.xpath(link_value_xpath)
+                                                   .extract()).strip())
+                tick_value = html.unescape(''.join(row.xpath(tick_value_path)
+                                                   .extract()).strip())
+
+                # If the value is actually nested in a link or img use that
+                # value
+                if link_value:
+                    value = link_value
+                elif tick_value:
+                    value = tick_value
+
+                product[key] = value
+
+        print(product["category"])
+        yield product
