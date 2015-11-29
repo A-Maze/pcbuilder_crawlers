fcaf145852b1d16e78478306552b3184b9bf52f1
Rik van der Werf <rikvanderwerf@outlook.com>
2015-11-29 15:07:41 +0100

    pipeline setup

--- pcbuilder_crawlers/pipelines.py
+++ pcbuilder_crawlers/pipelines.py
@@ -4,8 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import requests
 
 class PcbuilderCrawlersPipeline(object):
+
     def process_item(self, item, spider):
+        """ translate the json keys """
+
+        #make the request
+        url = 'http://95.85.12.99/' + item['category'] + '/product'
+        r = requests.post(url, item)
         return item
