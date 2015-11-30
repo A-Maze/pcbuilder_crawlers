4582e894469b499c5b20c690ebd31eafdbef17f1
Rik van der Werf <rikvanderwerf@outlook.com>
2015-11-30 17:37:58 +0100

    translate the keys from the crawled item to english

--- pcbuilder_crawlers/pipelines.py
+++ pcbuilder_crawlers/pipelines.py
@@ -1,4 +1,3 @@
-from spiders.hardware_info import HardwareInfoSpider
 from scrapy.settings import Settings
 from template import TemplateInterface
 
@@ -6,19 +5,12 @@ config = Settings()
 
 
 class Pipeline(object):
-    def __init__(self):
-        self.root_url = config.get("API_URL")
-        # self.api_url = ("{root_url}category/{category_name}/product"
-        #                 "/{product_id}" .format(root_url=self.root_url))
 
     def process_item(self, item, spider):
         template = TemplateInterface()
         category = template.translate_category(item['category'])
-        if spider is HardwareInfoSpider:
-            self.post_item(item)
-        else:
-            self.post_price(item)
-        return item
+        temp = template.get_template(category)
+        item = template.translate_item(temp, item)
 
     def post_item(self, item):
         return item
--- pcbuilder_crawlers/template.py
+++ pcbuilder_crawlers/template.py
@@ -1,10 +1,12 @@
 import json
-import os
+
 
 class TemplateInterface(object):
-    def get_template(key):
-            template = None
-            return template
+    def get_template(self, category):
+        path = 'templates/{category}.json'.format(category=category)
+        with open(path) as data:
+            template = json.load(data) or None
+        return template
 
     def translate_category(self, name):
         with open('templates/category.json') as data:
@@ -13,6 +15,17 @@ class TemplateInterface(object):
             categoryArray = [i.encode('UTF-8') if isinstance(i, basestring)
                              else i for i in categories[category_names]]
             if name in categoryArray:
-                print 'found'
                 return categoryArray[0]
         return None
+
+    def translate_item(self, template, item):
+        for row in item:
+            for other_row in template:
+                encoded_template_row = [i.encode('UTF-8')
+                                        if isinstance(i, basestring)
+                                        else i for i
+                                        in template[other_row]]
+                print other_row
+                if row in encoded_template_row:
+                    item[other_row] = item.pop(row)
+        return item
