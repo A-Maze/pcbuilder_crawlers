02bdee74b9c7bf398ef4682330b86868889042e5
Rik van der Werf <rikvanderwerf@outlook.com>
2015-12-19 13:13:21 +0100

    you can send the request now

--- pcbuilder_crawlers/pipelines.py
+++ pcbuilder_crawlers/pipelines.py
@@ -1,19 +1,26 @@
 from scrapy.settings import Settings
 from template import TemplateInterface
-
+import urllib2
+import json
 config = Settings()
 
 
 class Pipeline(object):
-
     def process_item(self, item, spider):
         template = TemplateInterface()
         category = template.translate_category(item['category'])
         temp = template.get_template(category)
         item = template.translate_item(temp, item)
+        json_item = json.dumps(item)
+        self.post_item(json_item, category)
 
-    def post_item(self, item):
-        return item
+    def post_item(self, item, category):
+        url = 'http://localhost/category/{}/product/'.format(category)
+        response = urllib2.Request(url, item)
+        response.add_header('Content-Type', 'application/json')
+        resp = urllib2.urlopen(response)
+        print resp
+        return
 
-    def post_price(self, item):
+    def post_price(self, item, category):
         return item
--- pcbuilder_crawlers/template.py
+++ pcbuilder_crawlers/template.py
@@ -4,28 +4,25 @@ import json
 class TemplateInterface(object):
     def get_template(self, category):
         path = 'templates/{category}.json'.format(category=category)
-        with open(path) as data:
+        with open(path, 'r') as data:
             template = json.load(data) or None
         return template
 
     def translate_category(self, name):
-        with open('templates/category.json') as data:
+        with open('templates/category.json', 'rb') as data:
             categories = json.load(data)
         for category_names in categories:
-            categoryArray = [i.encode('UTF-8') if isinstance(i, basestring)
-                             else i for i in categories[category_names]]
-            if name in categoryArray:
-                return categoryArray[0]
+            if name in categories[category_names]:
+                return categories[category_names][0]
         return None
 
     def translate_item(self, template, item):
+        """ this function checks for every row in the crawled item if the
+            key exists in the template. If this is the case the key gets
+            translated into English by adding a new key with the correct name
+            and then removing the old one """
         for row in item:
-            for other_row in template:
-                encoded_template_row = [i.encode('UTF-8')
-                                        if isinstance(i, basestring)
-                                        else i for i
-                                        in template[other_row]]
-                print other_row
-                if row in encoded_template_row:
-                    item[other_row] = item.pop(row)
+            for template_row in template:
+                if row in template[template_row]:
+                    item[template_row] = item.pop(row)
         return item
